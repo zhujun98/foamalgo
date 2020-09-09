8f383ab8748b3561b9ff4a6204eda8c4f6593252
zhujun98 <zhujun981661@gmail.com>
2020-09-09 22:57:02 +0200

    Add BSD license file

--- tests/test_blas.cpp
+++ tests/test_blas.cpp
@@ -1,7 +1,7 @@
 /**
  * Distributed under the terms of the BSD 3-Clause License.
  *
- * The full license is in the file LICENSE, distributed with this software.
+ * The full license is in the file LICENSE_BS, distributed with this software.
  *
  * Author: Jun Zhu <jun.zhu@xfel.eu>
  * Copyright (C) European X-Ray Free-Electron Laser Facility GmbH.
