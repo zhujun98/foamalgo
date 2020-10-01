2865f00e119b608a03cf9745f25befc75904c8b5
zhujun98 <zhujun981661@gmail.com>
2020-10-01 22:43:43 +0200

    Release 0.0.5

--- pyfoamalgo/__init__.py
+++ pyfoamalgo/__init__.py
@@ -6,7 +6,7 @@ The full license is in the file LICENSE, distributed with this software.
 Copyright (C) 2020, Jun Zhu. All rights reserved.
 """
 
-__version__ = "0.0.3"
+__version__ = "0.0.5"
 
 
 from .statistics import (
