3d8af5558d04d433beefbf1b5c337e63ae13902c
zhujun98 <zhujun981661@gmail.com>
2021-03-19 15:15:55 +0100

    Add syntex highlight in RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -80,6 +80,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 
