83c0c56a7f587b9c3cc4c8c670e2e4a073f25a53
Jun Zhu <zhujun981661@gmail.com>
2022-09-30 12:51:57 +0200

    Drop support for Python 3.7 and 3.8, add support for Python 3.10

--- setup.py
+++ setup.py
@@ -258,16 +258,15 @@ setup(
             'pytest',
         ],
     },
-    python_requires='>=3.7',
+    python_requires='>=3.9',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Scientific/Engineering :: Physics',
     ]
