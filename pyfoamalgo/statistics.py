5f68c1497a5bfb4d42684e24871add0e35be7d55
Jun Zhu <zhujun981661@gmail.com>
2022-10-04 14:13:21 +0200

    Replace @Copyright ... with Author ...

--- pyfoamalgo/statistics.py
+++ pyfoamalgo/statistics.py
@@ -3,7 +3,7 @@ Distributed under the terms of the GNU General Public License v3.0.
 
 The full license is in the file LICENSE, distributed with this software.
 
-Copyright (C) 2020, Jun Zhu. All rights reserved.
+Author: Jun Zhu
 """
 import math
 import numpy as np
